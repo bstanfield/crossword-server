3720c787c9cf2347d54b19359876b5869771ff54
Ben Stanfield <bnstnfld@gmail.com>
2020-11-28 16:39:48 -0800

    adding logs to fix heroku

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: '*'
+    origin: '*',
   }
 });
 
 let boardGuesses = [null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null, false, null, null, null, false, null, null, null, null, false, false, false, null, null, null, false, null, null, null, null, false, false, false, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, false, false, false, null, null, null, null, false, null, null, null, false, false, false, null, null, null, null, false, null, null, null, false, null, null, null, null, null, null, false, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null]
 
 io.on("connection", (socket) => {
+  console.log('Connection made!')
+  console.log('Flag 0')
   getBoardAndEmit(socket)
 
   socket.on('message', data => {
@@ -29,6 +31,7 @@ io.on("connection", (socket) => {
     boardGuesses[position - 1] = letter
     io.sockets.emit("FromAPI", boardGuesses);
   });
+  console.log('Flag 2')
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
@@ -38,6 +41,7 @@ io.on("connection", (socket) => {
 
 
 const getBoardAndEmit = socket => {
+  console.log('Flag 1')
   // Emitting a new message. Will be consumed by the client
   socket.emit("FromAPI", boardGuesses);
 };
