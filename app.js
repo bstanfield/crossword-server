3cd498361cbbef0bf5727884ff72447bb9af71ff
Ben Stanfield <bnstnfld@gmail.com>
2020-11-28 16:09:44 -0800

    successfully emits to all sockets

--- app.js
+++ app.js
@@ -19,24 +19,29 @@ const io = socketIo(server, {
   }
 });
 
-let interval
+let boardGuesses = ['t', 'a', null, null, null, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null, false, null, null, null, false, null, null, null, null, false, false, false, null, null, null, false, null, null, null, null, false, false, false, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, false, false, false, null, null, null, null, false, null, null, null, false, false, false, null, null, null, null, false, null, null, null, false, null, null, null, null, null, null, false, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null, null, null, null, null, false, null, null, null, null, false, null, null, null, null, null]
 
 io.on("connection", (socket) => {
   console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  getBoardAndEmit(socket)
+
+  socket.on('message', data => {
+    const { position, letter, iterator } = data
+    console.log('message received!', data)
+    boardGuesses[position - 1] = letter
+    io.sockets.emit("FromAPI", boardGuesses);
+  });
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
-    clearInterval(interval);
   });
 });
 
-const getApiAndEmit = socket => {
-  const response = new Date();
+
+
+const getBoardAndEmit = socket => {
   // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
+  socket.emit("FromAPI", boardGuesses);
 };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
