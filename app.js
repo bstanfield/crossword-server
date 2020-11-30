368f5c64887f2d5878fca0489bebe0ccd682546b
Ben Stanfield <bnstnfld@gmail.com>
2020-11-29 17:03:31 -0800

    lots of new functionality

--- app.js
+++ app.js
@@ -19,28 +19,60 @@ const io = socketIo(server, {
   }
 });
 
-let boardGuesses = ['', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', false, '', '', '', false, false, false, false, '', '', '', '', '', '', false, '', '', '', '', false, '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', false, '', '', '', '', false, '', '', '', '', '', '', false, false, false, false, '', '', '', false, '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '']
+const randomColors = ['#FCEE7F', '#FF2865', '#A13BE0', '#28CBFF']
+
+let boardGuesses = ['', '', '', '', false, '', '', '', '', '', false, '', '', '', '', '', '', '', '', false, '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', false, false, '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', false, '', '', '', '', false, false, false, '', '', '', '', '', '', '', '', false, false, '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', false, false, '', '', '', '', '', '', '', '', false, false, false, '', '', '', '', false, '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', false, false, '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', '', '', '', '', '', '', '', '', '', false, '', '', '', '', '', false, '', '', '', '', '', '', '', '', false, '', '', '', '', '', false, '', '', '', '']
+let clientsHighlights = {}
+let connectedClients = {}
 
 io.on("connection", (socket) => {
-  getBoardAndEmit(socket)
+  socket.emit('inputChange', boardGuesses);
+  console.log('New client: ', socket.id);
+
+  // Assigns a color for the client
+  connectedClients[socket.id] = randomColors[Math.floor(Math.random() * randomColors.length)]
+
+  // Tell all clients # of clients
+  io.emit('newPlayer', connectedClients)
 
   socket.on('message', data => {
-    const { position, letter, iterator } = data
-    boardGuesses[position - 1] = letter
-    console.log('Sending only to non-senders')
-    socket.broadcast.emit("FromAPI", boardGuesses);
+    console.log(`Client ${socket.id} sent a message.`);
+    const { type, value } = data
+
+    // Registers a square input letter change
+    if (type === 'input') {
+      const { position, letter, iterator } = value;
+
+      boardGuesses[position - 1] = letter;
+      socket.broadcast.emit('inputChange', boardGuesses);
+    }
+
+    // Sends highlight information for clients
+    if (type === 'newHighlight') {
+      const { id } = socket;
+      const color = connectedClients[id]
+      console.log('A player highlighted a new clue.')
+      clientsHighlights[id] = { squares: value, color }
+      console.log('sending this: ', clientsHighlights)
+      socket.broadcast.emit('newHighlight', clientsHighlights)
+    }
   });
+
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    console.log('~~~~~~~~~~~')
+    console.log(`${socket.id} disconnected`);
+    console.log('connected clients: ', connectedClients)
+    const clientToDelete = connectedClients[socket.id];
+    if (clientToDelete) {
+      console.log('Deleted client')
+      delete connectedClients[socket.id];
+      delete clientsHighlights[socket.id];
+      io.emit('newPlayer', connectedClients)
+      io.emit('newHighlight', clientsHighlights)
+      console.log('~~~~~~~~~~~')
+    }
   });
 });
-
-
-
-const getBoardAndEmit = socket => {
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", boardGuesses);
-};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
