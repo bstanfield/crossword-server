9ac5de93d244b5122dccea457f39f78b0c519ebb
Ben Stanfield <bnstnfld@gmail.com>
2020-12-06 12:22:57 -0800

    can successfully pass JSON results

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ const util = require("util");
 var myArgs = process.argv.slice(2);
 
 const findNewPuzzle = async (dayOfWeek) => {
-  let options = [];
   let years;
   let monthGroupings;
   let dayGroupings;
@@ -25,7 +24,6 @@ const findNewPuzzle = async (dayOfWeek) => {
           )
       )
     );
-    console.log("day groupings: ", dayGroupings);
     crosswords = await Promise.all(
       dayGroupings.map(
         async (yearGrouping, yearIndex) =>
@@ -44,52 +42,23 @@ const findNewPuzzle = async (dayOfWeek) => {
           )
       )
     );
-    return crosswords
   } catch (err) {
-    console.log("err: ", err);
+    console.log('err: ', err)
   }
-
-  // fs.readdir('./crosswords', 'utf8', (err, years) => {
-  //   if (err) {
-  //     return console.log('err: ', err)
-  //   }
-  //   years.map(year => {
-  //     fs.readdir(`./crosswords/${year}`, 'utf8', (err, months) => {
-  //       if (err) {
-  //         return console.log('err: ', err)
-  //       }
-  //       months.map(month => {
-  //         fs.readdir(`./crosswords/${year}/${month}`, 'utf8', (err, days) => {
-  //           if (err) {
-  //             return console.log('err: ', err)
-  //           }
-  //           days.map(day => {
-  //             fs.readFile(`./crosswords/${year}/${month}/${day}`, 'utf8', (err, crossword) => {
-  //               if (err) {
-  //                 return console.log('err: ', err)
-  //               }
-  //               try {
-  //                 const crosswordJSON = JSON.parse(crossword);
-  //                 if (dayOfWeek === crosswordJSON.dow) {
-  //                   options.push(crosswordJSON)
-  //                 }
-
-  //               } catch (err) {
-  //                 console.log('not in JSON format: ', `${year}/${month}/${day}`)
-  //               }
-  //             })
-  //           })
-  //         })
-  //       })
-  //     }
-  //     )
-  //   })
-  // })
-  return years;
+  return crosswords[0].flat()
 };
 
 const run = async () => {
   const results = await findNewPuzzle(myArgs[0]);
+  let jsonResults = []
+  results.map(result => {
+    try {
+      jsonResults.push(JSON.parse(result))
+    } catch (e) {
+      console.log('e: ', e, result)
+    }
+  })
+  console.log('json results: ', jsonResults)
 };
 
 run();
