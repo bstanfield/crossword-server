7427d90e4bb18161f23f3e158f1f9267bf586420
Ben Stanfield <bnstnfld@gmail.com>
2020-12-07 11:31:14 -0800

    new cw

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const instantiateGuesses = (grid) => grid.map(item => {
 })
 
 const getPuzzle = async () => {
-  // Hardcoded days for now
+  // Hardcoded day for now
   const board = await findNewPuzzle('Monday');
   const { grid } = board
   const guesses = instantiateGuesses(grid)
