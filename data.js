9424dcf74c304ca9962d9ad3961ce947bbd2d2ab
Ben Stanfield <bnstnfld@gmail.com>
2020-12-25 12:56:14 -0800

    no sundays for now

--- data.js
+++ data.js
@@ -7,7 +7,9 @@ const findNewPuzzle = async (dow) => {
   const cwData = await Promise.all(filePaths.map(fp => fs.readFile(fp, 'utf8')))
   const cwJSON = cwData.map(cw => JSON.parse(cw))
 
-  const dowCrosswords = cwJSON.filter(cw => cw.dow === dow)
+  const fifteenByFifteenCrosswords = cwJSON.filter(cw => cw.size.cols === 15 && cw.size.rows === 15)
+
+  const dowCrosswords = fifteenByFifteenCrosswords.filter(cw => cw.dow === dow)
   return dowCrosswords[Math.floor(Math.random() * dowCrosswords.length)];
 };
 
