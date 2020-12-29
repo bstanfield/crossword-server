4737cb3946a5fb3246260f4ec9fcd681e362d5d5
Ben Stanfield <bnstnfld@gmail.com>
2020-12-28 20:39:43 -0800

    ip

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: 'ec2-54-144-109-253.compute-1.amazonaws.com',
+    host: '54.144.109.253',
     user: 'jfecklpdzeravm',
     password: '37e9166ad928604062524e01f8e91bbdec0a1dd48cc37a76d76eda882c6ec5fc',
     database: 'd9hevhsicmpo59'
