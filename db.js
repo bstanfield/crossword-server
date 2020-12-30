e3ac940ee777a387812257596b3ccc12af1e5352
Ben Stanfield <bnstnfld@gmail.com>
2020-12-30 13:38:27 -0800

    conditional for choosing env

--- db.js
+++ db.js
@@ -6,13 +6,15 @@ const pgp = require('pg-promise')({
 });
 
 const cn = process.env.DATABASE_URL;
+const testing = process.env.TESTING;
 
 const db = pgp({
   connectionString: cn,
-  ssl: false,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: testing
+    ? false
+    : {
+      rejectUnauthorized: false
+    },
 });
 
 module.exports = {
