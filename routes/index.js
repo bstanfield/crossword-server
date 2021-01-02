6831b886661037a27ae17fa51fd0b62b04693a8d
Ben Stanfield <bnstnfld@gmail.com>
2021-01-02 10:06:37 -0800

    adding more keys

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
 
-const validKeys = ['alpha', 'beta', 'svalbard', 'circle_sm', 'new_york', 'mississippi', 'sunderman', 'albina', 'bonsai', 'black_jeep', '2802', 'tahoe', '3_nights', 'bird_watcher', 'not_the_continent', 'nipomo', 'gridflower', 'campfire', 'persimmon', 'talbot', 'banjo'];
+const validKeys = ['alpha', 'beta', 'svalbard', 'circle_sm', 'new_york', 'mississippi', 'sunderman', 'albina', 'bonsai', 'black_jeep', '2802', 'tahoe', '3_nights', 'bird_watcher', 'not_the_continent', 'nipomo', 'gridflower', 'campfire', 'persimmon', 'talbot', 'banjo', 'minions', 'beemsterboss'];
 
 router.get("/secret", (req, res) => {
   const key = req.query.key;
