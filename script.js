e650f07e43b390aec58ce96dae570350714b513d
Ben Stanfield <bnstnfld@gmail.com>
2023-04-29 19:09:41 -0700

    new puzzles

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const moment = require("moment");
 
 // Modify these dates to download new crosswords locally.
 
-var a = moment("2023-01-03");
-var b = moment("2023-03-07");
+var a = moment("2023-03-07");
+var b = moment("2023-04-29");
 
 async function init() {
   for (var m = moment(a); m.diff(b, "days") <= 0; m.add(1, "days")) {
