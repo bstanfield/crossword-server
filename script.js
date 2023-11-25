b5346acba9cf558076d8db6eacc73651c78673b2
bstanfield <bnstnfld@gmail.com>
2023-11-24 16:26:44 -0800

    New crossword puzzles added

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 const moment = require("moment");
+const { execSync } = require("child_process");
+require("dotenv").config();
 
-// Modify these dates to download new crosswords locally.
+// Configure Git for username and email
+try {
+  execSync('git config --global user.name "bstanfield"');
+  execSync('git config --global user.email "bnstnfld@gmail.com"');
+} catch (error) {
+  console.error("Error configuring Git:", error);
+}
 
-var a = moment("2023-11-17");
-var b = moment("2023-11-24");
+// Configure Git remote URL with PAT
+try {
+  const githubToken = process.env.GITHUB_TOKEN;
+  execSync(
+    `git remote set-url origin https://x-access-token:${githubToken}@github.com/bstanfield/crossword-server.git`
+  );
+} catch (error) {
+  console.error("Error setting up Git remote URL:", error);
+}
+
+// Define dates for fetching crosswords
+var startDate = moment("2023-11-17");
+var endDate = moment("2023-11-24");
 
 async function init() {
-  for (var m = moment(a); m.diff(b, "days") <= 0; m.add(1, "days")) {
-    const dateToUse = m.format("L");
-
+  for (
+    var m = moment(startDate);
+    m.diff(endDate, "days") <= 0;
+    m.add(1, "days")
+  ) {
+    const dateToUse = m.format("YYYY-MM-DD");
     let url =
       "https://www.xwordinfo.com/JSON/Data.ashx?format=text&date=" + dateToUse;
 
@@ -37,126 +59,50 @@ async function init() {
       },
     };
 
-    // Try disabling fetch and running a new fn to check what gaps there are.
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => {
-        const months = [
-          "01",
-          "02",
-          "03",
-          "04",
-          "05",
-          "06",
-          "07",
-          "08",
-          "09",
-          "10",
-          "11",
-          "12",
-        ];
+    try {
+      const response = await fetch(url, options);
+      const json = await response.json();
 
-        const year = new Date(json.date).getFullYear();
-        const month = new Date(json.date).getMonth();
-        const day = new Date(json.date).getDate();
+      const year = new Date(json.date).getFullYear();
+      const month = new Date(json.date).getMonth();
+      const day = new Date(json.date).getDate();
 
-        // console.log('Year: ', year);
-        // console.log('Month: ', months[month]);
-        // console.log('Day: ', day);
-
-        const findOrCreateDirectory = (year, month, day) => {
-          fs.access("./crosswords/" + year + "/" + month, function (error) {
-            if (error) {
-              console.log("Directory does not exist.");
-              console.log("Creating directory...");
-              fs.mkdirSync(
-                process.cwd() + "/crosswords/" + year + "/" + month,
-                { recursive: true },
-                (error) => {
-                  if (error) {
-                    console.error("An error occur: ", error);
-                  } else {
-                    console.log("Directory created");
-                  }
-                }
-              );
-
-              if (
-                fs.existsSync(
-                  "./crosswords/" + year + "/" + month + "/" + day + ".json"
-                )
-              ) {
-                console.log(m.format("L"), "File exists already.");
-              } else if (
-                fs.existsSync(
-                  "./crosswords/" +
-                    year +
-                    "/" +
-                    month +
-                    "/" +
-                    `0${day}` +
-                    ".json"
-                )
-              ) {
-                console.log(m.format("L"), "File exists already.");
-              } else {
-                fs.writeFile(
-                  "./crosswords/" + year + "/" + month + "/" + day + ".json",
-                  JSON.stringify(json),
-                  (error) => {
-                    if (error) {
-                      console.error("An error occur: ", error);
-                    } else {
-                      console.log("File added!", year, month, day);
-                    }
-                  }
-                );
-              }
-            } else {
-              if (
-                fs.existsSync(
-                  "./crosswords/" + year + "/" + month + "/" + day + ".json"
-                ) ||
-                fs.existsSync(
-                  "./crosswords/" +
-                    year +
-                    "/" +
-                    month +
-                    "/" +
-                    `0${day}` +
-                    ".json"
-                )
-              ) {
-                console.log(m.format("L"), "File exists already.");
-              } else {
-                fs.writeFile(
-                  "./crosswords/" + year + "/" + month + "/" + day + ".json",
-                  JSON.stringify(json),
-                  (error) => {
-                    if (error) {
-                      console.error("An error occur HERE: ", error);
-                    } else {
-                      console.log("File added!", year, month, day);
-                    }
-                  }
-                );
-              }
-            }
-          });
-        };
-
-        findOrCreateDirectory(year, months[month], day);
-      })
-      .catch((err) => console.error("error:" + err));
+      findOrCreateDirectory(year, month, day, json);
+    } catch (error) {
+      console.error("Error fetching or processing crossword data:", error);
+    }
 
     await sleep(2000);
+  }
+
+  // Push changes to GitHub
+  try {
+    execSync("git add .");
+    execSync('git commit -m "New crossword puzzles added"');
+    execSync("git push");
+  } catch (error) {
+    console.error("Failed to push to GitHub:", error);
+  }
+}
+
+function findOrCreateDirectory(year, month, day, json) {
+  const dirPath = `./crosswords/${year}/${String(month).padStart(2, "0")}`;
+  const filePath = `${dirPath}/${String(day).padStart(2, "0")}.json`;
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
+    console.log(`File added: ${filePath}`);
+  } else {
+    console.log(`File already exists: ${filePath}`);
   }
 }
 
 function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 init();
